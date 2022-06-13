07e0829252223054c76d3272d7592c35073f6996
daenamcclintock <daena.mcclintock@gmail.com>
2022-06-13 17:44:44 -0400

    updates to deploy scripts and hardhat config

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,12 +1,30 @@
-const { developmentChains } = require("../helper-hardhat-config")
+const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
 
-module.exports = async function({ getNamedAccounts, deployments }) {
+const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium. It costs 0.25 LINK as a base fee per request
+const GAS_PRICE_LINK = 1e9 // calculated value based on the gas price of the chain
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    const args = [BASE_FEE, GAS_PRICE_LINK]
 
-    if(developmentChains.includes(network.name)) {
-        log("Local netowkr detected! Deploying mocks...")
-        // deploy a mock VRFCoordinator
+    // If we are on a local development network, we need to deploy mocks!
+    if (chainId == 31337) {
+        log("Local network detected! Deploying mocks...")
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+
+        log("Mocks Deployed!")
+        log("----------------------------------------------------------")
+        log("You are deploying to a local network, you'll need a local network running to interact")
+        log(
+            "Please run `yarn hardhat console --network localhost` to interact with the deployed smart contracts!"
+        )
+        log("----------------------------------------------------------")
     }
 }
+module.exports.tags = ["all", "mocks"]
